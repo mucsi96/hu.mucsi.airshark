793aff4c86247e8635330a9e04fd867ef34fabac
mucsi <igor@mucsi>
2013-01-21 19:29:46 +0100

    Remove log before starting framework

--- airshark.assembly/src/main/filtered-resources/debug.bat
+++ airshark.assembly/src/main/filtered-resources/debug.bat
@@ -1,2 +1,3 @@
 rd /s /q configuration\org.eclipse.osgi
+del hello.areas.log
 java -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -Xdebug -jar org.eclipse.osgi-${org.eclipse.osgi}.jar -console
