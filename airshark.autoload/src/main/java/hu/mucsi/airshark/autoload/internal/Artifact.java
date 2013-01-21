0626e3edbb0f1200000db25c42207c795435fd00
mucsi <igor@mucsi>
2013-01-21 19:12:55 +0100

    Autoloader recognizes pom version and automaticly delete old versions

--- airshark.autoload/src/main/java/hu/mucsi/airshark/autoload/internal/Artifact.java
+++ airshark.autoload/src/main/java/hu/mucsi/airshark/autoload/internal/Artifact.java
@@ -3,13 +3,28 @@ package hu.mucsi.airshark.autoload.internal;
 import hu.mucsi.airshark.autoload.ArtifactListener;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.jar.JarInputStream;
+import java.util.jar.Manifest;
+
+import org.osgi.framework.BundleException;
+import org.osgi.framework.Constants;
+import org.osgi.framework.Version;
 
 
 public class Artifact
 {
 
-    private File path;
+    @Override
+	public String toString() {
+		return symbolicName + version;
+	}
+
+	private File path;
     private File jaredDirectory;
     private URL jaredUrl;
     private ArtifactListener listener;
@@ -17,8 +32,26 @@ public class Artifact
     private File transformed;
     private long bundleId = -1;
     private long checksum;
+    private String symbolicName;
+    private Version version;
 
-    public File getPath()
+    public String getSymbolicName() {
+		return symbolicName;
+	}
+
+	public void setSymbolicName(String symbolicName) {
+		this.symbolicName = symbolicName;
+	}
+
+	public Version getVersion() {
+		return version;
+	}
+
+	public void setVersion(Version version) {
+		this.version = version;
+	}
+
+	public File getPath()
     {
         return path;
     }
@@ -96,5 +129,29 @@ public class Artifact
     public void setChecksum(long checksum)
     {
         this.checksum = checksum;
+    }
+    
+    public void loadProps() throws IOException, BundleException {
+    	InputStream is = new FileInputStream(transformed != null ? transformed : path);
+        try
+        {
+        	is.mark(256 * 1024);
+            JarInputStream jar = new JarInputStream(is);
+            Manifest m = jar.getManifest();
+            if( m == null ) {
+                throw new BundleException(
+                    "The bundle " + path + " does not have a META-INF/MANIFEST.MF! "+
+                        "Make sure, META-INF and MANIFEST.MF are the first 2 entries in your JAR!");
+            }
+            String sn = m.getMainAttributes().getValue(Constants.BUNDLE_SYMBOLICNAME);
+            String vStr = m.getMainAttributes().getValue(Constants.BUNDLE_VERSION);
+            Version v = vStr == null ? Version.emptyVersion : Version.parseVersion(vStr);
+            this.setSymbolicName(sn);
+            this.setVersion(v);
+        }
+        finally
+        {
+            is.close();
+        }
     }
 }
